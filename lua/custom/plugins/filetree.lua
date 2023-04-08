aa31fee684b54546198aba47e7235f8bf0f46077
Nicolás Leal <lealnicolas96@gmail.com>
2023-04-08 19:44:45 -0400

    Handle NeoTree as a file explorer

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -9,9 +9,25 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
-    require('neo-tree').setup {}
+  config = function()
+    require('neo-tree').setup {
+      close_if_last_window = false,
+      window = {
+        position = "right",
+      },
+      event_handlers = {
+        -- Auto close when open a file
+        {
+          event = "file_opened",
+          handler = function()
+            require("neo-tree").close_all()
+          end
+        },
+
+      }
+    }
+
+    -- Keymap for open and close file explorer
+    vim.keymap.set('n', '<leader>nt', ':Neotree toggle<CR>', { desc = 'Toggle file explorer' })
   end,
 }
-
-
