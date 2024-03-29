72c6879a95a3a83bbfb1718c7a5ae371cd3e0c0d
Nicolás Leal <lealnicolas96@gmail.com>
2024-03-29 11:11:29 -0400

    Remove keymaps and use TmuxNavigator plugin to switch between panes

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -10,10 +10,10 @@ vim.keymap.set('n', '<leader>w', ':w<CR>')
 vim.keymap.set('n', '<leader>q', ':q<CR>')
 
 -- Keymaps for navigate through split screens
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+-- vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+-- vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+-- vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+-- vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
 
 -- Keymaps for navigate through tabs
 vim.keymap.set('n', '<leader>h', ':tabprevious<CR>')
