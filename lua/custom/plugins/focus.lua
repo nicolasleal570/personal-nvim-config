20bf3d219e980474967dfd64c29734d80a2c3d25
Nicolas Leal <lealnicolas96@gmail.com>
2026-06-08 15:07:43 -0400

    feat(plugins): add focus mode (zen-mode + twilight)
    Combo by folke for deep work:
    - twilight: dims code outside current treesitter scope (function/method/if/table)
    - zen-mode: hides status bar, line numbers, and centers the buffer; auto-activates twilight
    
    Keymaps:
    - <leader>z  toggle Zen mode (full focus + dimming)
    - <leader>tw toggle Twilight only (just dimming)

---
+++ lua/custom/plugins/focus.lua
@@ -0,0 +1,53 @@
+-- Focus mode: zen-mode + twilight.
+-- <leader>z toggles zen-mode, which auto-activates twilight (dimming surrounding code).
+
+vim.pack.add {
+  'https://github.com/folke/twilight.nvim',
+  'https://github.com/folke/zen-mode.nvim',
+}
+
+require('twilight').setup {
+  dimming = {
+    alpha = 0.25,
+    inactive = false,
+  },
+  context = 10,
+  treesitter = true,
+  expand = {
+    'function',
+    'method',
+    'table',
+    'if_statement',
+  },
+  exclude = {},
+}
+
+require('zen-mode').setup {
+  window = {
+    backdrop = 0.95,
+    width = 120,
+    height = 1,
+    options = {
+      number = false,
+      relativenumber = false,
+      cursorline = false,
+      cursorcolumn = false,
+      foldcolumn = '0',
+      list = false,
+    },
+  },
+  plugins = {
+    options = {
+      enabled = true,
+      ruler = false,
+      showcmd = false,
+      laststatus = 0,
+    },
+    twilight = { enabled = true },
+    gitsigns = { enabled = false },
+    tmux = { enabled = false },
+  },
+}
+
+vim.keymap.set('n', '<leader>z', '<CMD>ZenMode<CR>', { desc = 'Toggle [Z]en mode (focus)' })
+vim.keymap.set('n', '<leader>tw', '<CMD>Twilight<CR>', { desc = 'Toggle [T]wilight (dim only)' })
